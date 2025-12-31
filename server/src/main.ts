88d53115fa9d365aceaea78f71a4460791ad4e18
Brijesh <softbrijeshio@gmail.com>
2026-01-01 00:46:32 +0530

    feat(server): add cors security

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,13 +1,23 @@
 import "dotenv/config";
 import express, { type Response, type Request } from "express";
 import { pino } from "pino";
+import cors from "cors";
 import { routes } from "@/routes";
+
+const hosts = process.env.HOSTS_URI as string;
+const allowHosts = hosts?.split(" ");
 
 const app = express();
 const logger = pino();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: allowHosts,
+    credentials: true,
+  })
+);
 
 app.get("/", (_: Request, res: Response) => {
   res.send("Welcome to Study Tool AI");
