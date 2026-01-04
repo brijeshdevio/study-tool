39ab023e310fed4f8ac25c3962af1c5d3bf95b8f
Brijesh <softbrijeshio@gmail.com>
2026-01-04 20:55:05 +0530

    chore(server): setup express app

---
+++ server/src/main.ts
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the StudyTool API");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
