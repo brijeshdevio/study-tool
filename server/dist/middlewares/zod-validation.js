8064a508f76b7e6e5e53708ec2ae11a4987a0b40
Brijesh <softbrijeshio@gmail.com>
2026-01-05 16:18:46 +0530

    fix(api): fix directory stu

--- server/dist/middlewares/zod-validation.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.zodValidation = void 0;
-const zodValidation = (schema, source = "body") => async (req, _, next) => {
-    const data = req[source];
-    await schema.parseAsync(data);
-    next();
-};
-exports.zodValidation = zodValidation;
