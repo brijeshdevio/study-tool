0abc5ee80a703487bff6f9c30f6c7206f7f3dd16
Brijesh <softbrijeshio@gmail.com>
2026-01-05 16:43:42 +0530

    refactor(server): only added entry file

--- src/main.ts
+++ src/main.ts
@@ -1,53 +1,13 @@
-import "module-alias/register";
 import "dotenv/config";
-import express, { type Response, type Request } from "express";
-import { pino } from "pino";
-import cors from "cors";
-import rateLimit from "express-rate-limit";
-import { routes } from "@/routes";
-import { globalError } from "@/middlewares";
-
-const hosts = process.env.HOSTS_URI as string;
-const allowHosts = hosts?.split(" ");
+import express, { type Response } from "express";
 
 const app = express();
-const logger = pino();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: allowHosts,
-    credentials: true,
-  })
-);
-app.use(
-  rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 50, // Limit each IP to 50 requests per windowMs
-    message: "Too many requests from this IP, please try again later.",
-  })
-);
-
-app.get("/", (_: Request, res: Response) => {
-  res.send("Welcome to Study Tool AI");
+app.get("/", (_, res: Response) => {
+  res.send("Welcome to the StudyTool API");
 });
 
-app.get("/health", (_: Request, res: Response) => {
-  res.send("OK");
-});
-
-app.use("/api", routes);
-
-app.use(globalError);
-
-// for development
-// if (process.env.NODE_ENV === "development") {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
-// }
-//
-// // for vercel
-// export default app;
--- src/middlewares/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./global-error";
-export * from "./zod-validation";
